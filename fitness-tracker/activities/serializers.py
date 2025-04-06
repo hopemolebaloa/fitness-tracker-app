c684b68b0349d54848a2f7cfc84e585ccf939f1e
Hope Molebaloa <128815478+hopemolebaloa@users.noreply.github.com>
2025-04-06 09:39:12 +0000

    done

--- fitness-tracker/activities/serializers.py
+++ fitness-tracker/activities/serializers.py
@@ -1,19 +1,13 @@
-f# activities/serializers.py
+# activities/serializers.py
 from rest_framework import serializers
 from .models import Activity, ActivityType
 
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = '__all__'
+
 class ActivityTypeSerializer(serializers.ModelSerializer):
-    """Serializer for activity types"""
     class Meta:
         model = ActivityType
         fields = '__all__'
-
-class ActivitySerializer(serializers.ModelSerializer):
-    """Serializer for activities"""
-    activity_type_name = serializers.ReadOnlyField(source='activity_type.name')
-    
-    class Meta:
-        model = Activity
-        fields = ['id', 'activity_type', 'activity_type_name', 'duration', 'distance', 
-                 'calories_burned', 'date', 'notes', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'created_at', 'updated_at']
