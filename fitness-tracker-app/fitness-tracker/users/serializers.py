05cfbb498ffe3bc69e54dcebc95493e6a01c6a14
Hope Molebaloa <128815478+hopemolebaloa@users.noreply.github.com>
2025-04-06 20:51:03 +0000

    yea

--- fitness-tracker-app/fitness-tracker/users/serializers.py
+++ fitness-tracker-app/fitness-tracker/users/serializers.py
@@ -1,4 +1,5 @@
 # users/serializers.py
+# users/serializers.py
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
@@ -6,16 +7,17 @@ from django.contrib.auth.password_validation import validate_password
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    """Serializer for the User model"""
+    """Serializer for creating users"""
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        fields = ('id', 'username', 'email', 'password', 'password2', 'first_name', 'last_name')
         extra_kwargs = {
-            'first_name': {'required': False},
-            'last_name': {'required': False}
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'email': {'required': True}
         }
 
     def validate(self, attrs):
@@ -28,9 +30,9 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(**validated_data)
         return user
 
-class UserDetailsSerializer(serializers.ModelSerializer):
-    """Serializer for retrieving user details"""
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializer for user profile viewing and updating"""
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name')
-        read_only_fields = ('id',)
+        read_only_fields = ('id', 'email')
