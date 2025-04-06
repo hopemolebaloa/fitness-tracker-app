1c6da3e42c2f5ca294da44e1c7ec62d9047c7bf8
Hope Molebaloa <128815478+hopemolebaloa@users.noreply.github.com>
2025-04-06 08:45:43 +0000

    admin

--- fitness-tracker/activities/admin.py
+++ fitness-tracker/activities/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Activity, ActivityType
 
-# Register your models here.
+@admin.register(ActivityType)
+class ActivityTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('user', 'activity_type', 'duration', 'distance', 'calories_burned', 'date')
+    list_filter = ('activity_type', 'date', 'user')
+    search_fields = ('user__username', 'user__email', 'activity_type__name', 'notes')
+    date_hierarchy = 'date'
--- fitness-tracker/core/admin.py
+++ fitness-tracker/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Goal
 
-# Register your models here.
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'activity_type', 'target_value', 'metric', 'period', 'start_date', 'end_date', 'is_completed')
+    list_filter = ('is_completed', 'period', 'metric', 'activity_type')
+    search_fields = ('user__username', 'user__email', 'title', 'activity_type__name')
+    date_hierarchy = 'created_at'
