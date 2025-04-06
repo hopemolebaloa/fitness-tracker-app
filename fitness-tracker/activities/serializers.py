2db606282b9ab01794398fa386c4655197867d26
Hope Molebaloa <128815478+hopemolebaloa@users.noreply.github.com>
2025-04-06 09:25:09 +0000

    try

--- fitness-tracker/activities/serializers.py
+++ fitness-tracker/activities/serializers.py
@@ -1,31 +1,19 @@
+f# activities/serializers.py
 from rest_framework import serializers
 from .models import Activity, ActivityType
 
 class ActivityTypeSerializer(serializers.ModelSerializer):
-    """Serializer for the ActivityType model"""
+    """Serializer for activity types"""
     class Meta:
         model = ActivityType
-        fields = ('id', 'name', 'description')
+        fields = '__all__'
 
 class ActivitySerializer(serializers.ModelSerializer):
-    """Serializer for the Activity model"""
+    """Serializer for activities"""
     activity_type_name = serializers.ReadOnlyField(source='activity_type.name')
     
     class Meta:
         model = Activity
-        fields = ('id', 'activity_type', 'activity_type_name', 'duration', 'distance', 
-                 'distance_unit', 'calories_burned', 'date', 'notes', 'created_at')
-        read_only_fields = ('id', 'created_at')
-    
-    def create(self, validated_data):
-        # Set the user to the current authenticated user
-        validated_data['user'] = self.context['request'].user
-        return super().create(validated_data)
-
-class ActivitySummarySerializer(serializers.Serializer):
-    """Serializer for activity summary metrics"""
-    total_duration = serializers.IntegerField()
-    total_distance = serializers.FloatField()
-    total_calories = serializers.IntegerField()
-    total_activities = serializers.IntegerField()
-    timeframe = serializers.CharField()
+        fields = ['id', 'activity_type', 'activity_type_name', 'duration', 'distance', 
+                 'calories_burned', 'date', 'notes', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
--- fitness-tracker/activities/urls.py
+++ fitness-tracker/activities/urls.py
@@ -1,11 +1,13 @@
+# activities/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ActivityViewSet, ActivityTypeViewSet
+from .views import ActivityViewSet, ActivityTypeListView, activity_metrics
 
 router = DefaultRouter()
 router.register(r'activities', ActivityViewSet, basename='activity')
-router.register(r'activity-types', ActivityTypeViewSet, basename='activity-type')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('activities/types/', ActivityTypeListView.as_view(), name='activity-types'),
+    path('activities/metrics/', activity_metrics, name='activity-metrics'),
 ]
